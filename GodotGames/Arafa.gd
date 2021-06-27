b91546cdc9823eba990aa318d117f7af78ca8b79
Arafa42 <s107661@ap.be>
2021-06-27 20:40:03 +0200

    jumping added

--- GodotGames/Arafa.gd
+++ GodotGames/Arafa.gd
@@ -1,13 +1,20 @@
 extends KinematicBody2D
 
 var velocity = Vector2(0,0)
+const SPEED = 200
+const GRAVITY = 45
+const JUMPFORCE = -1200
 
 func _physics_process(delta):
 	if Input.is_action_pressed("left"):
-		velocity.x = -100
+		velocity.x = -SPEED
 	if Input.is_action_pressed("right"):
-		velocity.x = 100
+		velocity.x = SPEED
 		
-	move_and_slide(velocity)
-	
+	velocity.y = velocity.y +  GRAVITY
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMPFORCE
+		
+		
+	velocity = move_and_slide(velocity,Vector2.UP)
 	velocity.x = lerp(velocity.x,0,0.3)
